b008165e20aa58eff4d70efccf3a4e55f2ab170f
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-04-28 11:08:53 +0200

    new `list_examples` function

--- x_xy/io/__init__.py
+++ x_xy/io/__init__.py
@@ -3,7 +3,17 @@ from .parse_system import parse_system
 
 
 def load_example(name: str):
+    "Load example from examples dir. `name` is without .xml extension."
     from pathlib import Path
 
     xml_path = Path(__file__).parent.joinpath(f"examples/{name}.xml")
     return load_sys_from_xml(xml_path)
+
+
+def list_examples() -> list[str]:
+    import os
+    from pathlib import Path
+
+    examples_dir = Path(__file__).parent.joinpath("examples")
+
+    return [ex.split(".")[0] for ex in os.listdir(examples_dir)]
