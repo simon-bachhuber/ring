c53c302f716fec25f488ab7e9d88a3bde8cb2d55
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-06-09 23:29:18 +0200

    adds custom_jvp rule for normalize

--- x_xy/maths/safe.py
+++ x_xy/maths/safe.py
@@ -30,3 +30,27 @@ def safe_normalize(x):
         lambda x: x / jnp.where(is_zero, 1.0, safe_norm(x)),
         x,
     )
+
+
+@jax.custom_jvp
+def safe_normalize_custom_jvp(x):
+    """Execution- and Grad-safe for x=0.0. Normalizes along last axis."""
+    assert x.ndim == 1
+
+    is_zero = jnp.allclose(x, 0.0)
+    return jax.lax.cond(
+        is_zero,
+        lambda x: jnp.zeros_like(x),
+        lambda x: x / jnp.where(is_zero, 1.0, safe_norm(x)),
+        x,
+    )
+
+
+@safe_normalize_custom_jvp.defjvp
+def safe_normalize_custom_jvp_jvp(primals, tangents):
+    (x,) = primals
+    (x_dot,) = tangents
+    ans = safe_normalize_custom_jvp(x)
+    eps = 1e-5
+    ans_dot = (jnp.clip(1 / (jnp.linalg.norm(x) + eps), 1e-4, 1)) * x_dot
+    return ans, ans_dot
