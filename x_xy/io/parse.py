ad4ce676d2100bf87e506f41fe2565b18fb3e191
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-06-23 19:34:02 +0200

    bugfix;Temporarily remove one check in `parse_sys`

--- x_xy/io/parse.py
+++ x_xy/io/parse.py
@@ -16,7 +16,12 @@ def parse_system(sys: base.System) -> base.System:
         assert isinstance(name, str)
 
     for geom in sys.geoms:
-        assert geom.link_idx in list(range(sys.num_links())) + [-1]
+        # TODO
+        # This conflicts which how `sys_composer.delete_subsystem` works
+        # a simple way of solving this, is by making Geoms point to links
+        # by name and not by index
+        pass
+        # assert geom.link_idx in list(range(sys.num_links())) + [-1]
 
     inertia = _parse_system_calculate_inertia(sys)
     sys = sys.replace(links=sys.links.replace(inertia=inertia))
