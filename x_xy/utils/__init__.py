3c6b673f055207e78da41842e44c3efc40023b92
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-07-09 12:38:00 +0200

    add new module `pipeline` which holds high-level functions to perform certain steps in the enitre end-to-end pipeline

--- x_xy/utils/__init__.py
+++ x_xy/utils/__init__.py
@@ -1,3 +1,4 @@
+from . import pipeline
 from .batchsize import distribute_batchsize, expand_batchsize, merge_batchsize
 from .path import parse_path
 from .sys_composer import delete_subsystem, inject_system, morph_system
