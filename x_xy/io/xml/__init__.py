d85042d73b28d04d5676dab1f7f0b11802b7bc63
Marcel Schöckel <marcel.schoeckel@gmail.com>
2023-07-10 16:37:48 +0200

    add save_sys_to_xml_str and tests

--- x_xy/io/xml/__init__.py
+++ x_xy/io/xml/__init__.py
@@ -1,1 +1,2 @@
 from x_xy.io.xml.from_xml import load_sys_from_str, load_sys_from_xml
+from x_xy.io.xml.to_xml import save_sys_to_xml_str
---
+++ x_xy/io/xml/to_xml.py
@@ -0,0 +1,75 @@
+import jax.numpy as jnp
+
+
+from x_xy import base
+from x_xy.io.xml import abstract
+from xml.etree.ElementTree import Element, SubElement, tostring
+from xml.dom.minidom import parseString
+
+def save_sys_to_xml_str(sys: base.System) -> str:
+    def _to_str(obj):
+        if isinstance(obj, jnp.ndarray):
+            if obj.ndim == 0:
+                return str(obj)
+            return " ".join([str(x) for x in obj])
+        else:
+            return str(obj)
+
+    global_index_map = {qd: sys.idx_map(qd) for qd in ["q", "d"]}
+
+    # Create root element
+    x_xy = Element("x_xy")
+    x_xy.set("model", sys.model_name)
+
+    options = SubElement(x_xy, "options")
+    options.set("dt", str(sys.dt))
+    options.set("gravity", _to_str(sys.gravity))
+
+    # Create worldbody
+    worldbody = SubElement(x_xy, "worldbody")
+
+    def process_link(link_idx: int, parent_elem: Element):
+        link = sys.links[link_idx]
+        link_typ = sys.link_types[link_idx]
+        link_name = sys.link_names[link_idx]
+
+        # Create body element
+        body = SubElement(parent_elem, "body")
+        body.set("joint", link_typ)
+        body.set("name", link_name)
+
+        # Set attributes
+        abstract.AbsTrans.to_xml(body, link.transform1)
+        abstract.AbsPosMinMax.to_xml(body, link.pos_min, link.pos_max)
+        abstract.AbsDampArmaStiffZero.to_xml(
+            body,
+            sys.link_damping[global_index_map["d"][link_name]],
+            sys.link_armature[global_index_map["d"][link_name]],
+            sys.link_spring_stiffness[global_index_map["d"][link_name]],
+            sys.link_spring_zeropoint[global_index_map["d"][link_name]],
+            base.Q_WIDTHS[link_typ],
+            base.QD_WIDTHS[link_typ],
+            link_typ,
+        )
+
+        # Add geometry elements
+        geoms = sys.geoms
+        for geom in geoms:
+            if geom.link_idx == link_idx:
+                print(type(geom))
+                geom_elem = SubElement(body, "geom")
+                abstract_class = abstract.geometry_to_abstract[type(geom)]
+                abstract_class.to_xml(geom_elem, geom)
+
+        # Recursively process child links
+        for child_idx, parent_idx in enumerate(sys.link_parents):
+            if parent_idx == link_idx:
+                process_link(child_idx, body)
+
+    for root_link_idx, parent_idx in enumerate(sys.link_parents):
+        if parent_idx == -1:
+            process_link(root_link_idx, worldbody)
+
+    # Pretty print xml
+    xml_str = parseString(tostring(x_xy)).toprettyxml(indent="  ")
+    return xml_str
