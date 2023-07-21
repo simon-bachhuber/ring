c953c700b32a5c4b7ab1632ee2b3e406e3b5fbf2
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-07-21 14:20:51 +0200

    adds `replace_rr` in `exp_data.load_sys`

--- x_xy/subpkgs/exp_data/__init__.py
+++ x_xy/subpkgs/exp_data/__init__.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import Optional
+from typing import Callable, Optional
 
 import joblib
 import yaml
@@ -24,13 +24,30 @@ def _read_yaml(exp_id):
     return yaml_str
 
 
+def _replace_rxyz_with_rr(sys: x_xy.base.System):
+    return sys.replace(
+        link_types=[
+            "rr" if (typ in ["rx", "ry", "rz"]) else typ for typ in sys.link_types
+        ]
+    )
+
+
 def load_sys(
     exp_id: str,
+    preprocess_sys: Optional[Callable] = None,
     morph_yaml_key: Optional[str] = None,
     delete_after_morph: Optional[list[str]] = None,
+    replace_rxyz_with_rr: bool = False,
 ) -> x_xy.base.System:
     xml_path = _load_file_path(exp_id, "xml")
     sys = x_xy.io.load_sys_from_xml(xml_path)
+
+    if preprocess_sys is not None:
+        sys = preprocess_sys(sys)
+
+    if replace_rxyz_with_rr:
+        x_xy.algorithms.register_rr_joint()
+        sys = _replace_rxyz_with_rr(sys)
 
     if morph_yaml_key is not None:
         new_parents = _read_yaml(exp_id)["morph"][morph_yaml_key]
