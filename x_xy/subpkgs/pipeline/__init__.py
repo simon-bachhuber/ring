ae1d6195fb73dd7488c99fc74e749bb90585f39b
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-09-09 00:38:35 +0200

    rr logic is now fully automatic

--- x_xy/subpkgs/pipeline/__init__.py
+++ x_xy/subpkgs/pipeline/__init__.py
@@ -1,8 +1,10 @@
 from .generator import make_generator
-from .generator import make_offline_generator
 from .load_data import autodetermine_imu_names
 from .load_data import imu_data
 from .load_data import joint_axes_data
 from .load_data import load_data
 from .load_data import make_sys_noimu
 from .predict import predict
+from .rr_joint import register_rr_joint
+
+register_rr_joint()
---
+++ x_xy/subpkgs/pipeline/rr_joint.py
@@ -0,0 +1,32 @@
+import jax
+import jax.numpy as jnp
+
+import x_xy
+from x_xy import maths
+from x_xy.algorithms.jcalc import _draw_rxyz
+
+
+def register_rr_joint():
+    def _rr_transform(q, params):
+        def _rxyz_transform(q, _, axis):
+            q = jnp.squeeze(q)
+            rot = maths.quat_rot_axis(axis, q)
+            return x_xy.Transform.create(rot=rot)
+
+        return _rxyz_transform(q, None, params)
+
+    rr_joint = x_xy.JointModel(_rr_transform, rcmg_draw_fn=_draw_rxyz)
+    try:
+        x_xy.register_new_joint_type("rr", rr_joint, 1, 0)
+    except AssertionError:
+        pass
+
+
+def setup_fn_randomize_joint_axes(key, sys: x_xy.System) -> x_xy.System:
+    joint_axes = _draw_random_joint_axis(jax.random.split(key, sys.num_links()))
+    return sys.replace(links=sys.links.replace(joint_params=joint_axes))
+
+
+@jax.vmap
+def _draw_random_joint_axis(key):
+    return maths.rotate(jnp.array([1.0, 0, 0]), maths.quat_random(key))
