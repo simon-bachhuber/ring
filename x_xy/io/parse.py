38bf932fd254ac8c23ab5ffb45a5097a7453a3af
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-09-23 15:12:39 +0200

    parse now checks that all pos_min <= pos_max, and sorts geoms in ascending order

--- x_xy/io/parse.py
+++ x_xy/io/parse.py
@@ -1,3 +1,6 @@
+import jax
+import jax.numpy as jnp
+
 from .. import base
 from ..scan import scan_sys
 
@@ -9,12 +12,24 @@ def parse_system(sys: base.System) -> base.System:
     - populate the spatial inertia tensors
     - check that all names are unique
     - check that names are strings
+    - check that all pos_min <= pos_max
+    - order geoms in ascending order based on their parent link idx
     """
     assert len(sys.link_parents) == len(sys.link_types) == sys.links.batch_dim()
 
-    for name in sys.link_names:
-        assert sys.link_names.count(name) == 1, "Duplicated name in system"
+    for i, name in enumerate(sys.link_names):
+        assert sys.link_names.count(name) == 1, f"Duplicated name=`{name}` in system"
         assert isinstance(name, str)
+
+        pos_min, pos_max = sys.links.pos_min[i], sys.links.pos_max[i]
+
+        def print_warning():
+            jax.debug.print(
+                f"Warning from `parse_system`: name={name}, "
+                f"pos_min={pos_min}, pos_max={pos_max}"
+            )
+
+        jax.lax.cond(~jnp.all(pos_min <= pos_max), print_warning, lambda: None)
 
     for geom in sys.geoms:
         assert geom.link_idx in list(range(sys.num_links())) + [-1]
@@ -22,10 +37,15 @@ def parse_system(sys: base.System) -> base.System:
     inertia = _parse_system_calculate_inertia(sys)
     sys = sys.replace(links=sys.links.replace(inertia=inertia))
 
+    # sort geoms in ascending order
+    geoms = sys.geoms.copy()
+    geoms.sort(key=lambda geom: geom.link_idx)
+    sys = sys.replace(geoms=geoms)
+
     return sys
 
 
-def inertia_from_geometries(geometries: list[base.Geometry]) -> base.Inertia:
+def _inertia_from_geometries(geometries: list[base.Geometry]) -> base.Inertia:
     inertia = base.Inertia.zero()
     for geom in geometries:
         inertia += base.Inertia.create(geom.mass, geom.transform, geom.get_it_3x3())
@@ -39,7 +59,7 @@ def _parse_system_calculate_inertia(sys: base.System):
             if geom.link_idx == link_idx:
                 geoms_link.append(geom)
 
-        it = inertia_from_geometries(geoms_link)
+        it = _inertia_from_geometries(geoms_link)
         return it
 
     return scan_sys(sys, compute_inertia_per_link, "l", list(range(sys.num_links())))
