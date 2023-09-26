4af2205a5b6103becd7338c19915c8abb1b0c43e
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-09-26 13:19:24 +0200

    adds `setup_colab_env`

--- x_xy/utils/__init__.py
+++ x_xy/utils/__init__.py
@@ -2,6 +2,7 @@ from .batchsize import backend
 from .batchsize import distribute_batchsize
 from .batchsize import expand_batchsize
 from .batchsize import merge_batchsize
+from .colab import setup_colab_env
 from .path import parse_path
 from .utils import sys_compare
 from .utils import to_list
---
+++ x_xy/utils/colab.py
@@ -0,0 +1,42 @@
+import os
+import subprocess
+
+
+def setup_colab_env():
+    "Copied and modified from the getting-started-notebook of mujoco."
+    try:
+        from google.colab import files  # noqa: F401
+    except ImportError:
+        return
+
+    if subprocess.run("nvidia-smi").returncode:
+        raise RuntimeError(
+            "Cannot communicate with GPU. "
+            "Make sure you are using a GPU Colab runtime. "
+            "Go to the Runtime menu and select Choose runtime type."
+        )
+
+    # Add an ICD config so that glvnd can pick up the Nvidia EGL driver.
+    # This is usually installed as part of an Nvidia driver package, but the Colab
+    # kernel doesn't install its driver via APT, and as a result the ICD is missing.
+    # (https://github.com/NVIDIA/libglvnd/blob/master/src/EGL/icd_enumeration.md)
+    NVIDIA_ICD_CONFIG_PATH = "/usr/share/glvnd/egl_vendor.d/10_nvidia.json"
+    if not os.path.exists(NVIDIA_ICD_CONFIG_PATH):
+        with open(NVIDIA_ICD_CONFIG_PATH, "w") as f:
+            f.write(
+                """{
+            "file_format_version" : "1.0.0",
+            "ICD" : {
+                "library_path" : "libEGL_nvidia.so.0"
+            }
+        }
+        """
+            )
+
+
+# Configure MuJoCo to use the EGL rendering backend (requires GPU)
+os.environ["MUJOCO_GL"] = "egl"
+
+# install mediapy
+os.system("command -v ffmpeg >/dev/null || (apt update && apt install -y ffmpeg)")
+os.system("pip install -q mediapy")
