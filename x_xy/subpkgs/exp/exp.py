96fc8ac99fc8f8189b6e39150a9f2536d5a27c20
Simon Bachhuber <Simon.Bachhuber@gmx.net>
2023-10-13 10:48:38 +0200

    updates S_06.joblib file

--- x_xy/subpkgs/exp/exp.py
+++ x_xy/subpkgs/exp/exp.py
@@ -8,7 +8,10 @@ import tree_utils
 import yaml
 
 import x_xy
+from x_xy import maths
+from x_xy.io import load_comments_from_str
 from x_xy.io import load_comments_from_xml
+from x_xy.subpkgs import omc
 from x_xy.subpkgs import sys_composer
 
 _id2xml = {"S_06": "setups/arm.xml"}
@@ -34,25 +37,12 @@ def _replace_rxyz_with(sys: x_xy.base.System, replace_with: str):
 
 
 def _morph_new_parents_from_xml_file(file_path: str) -> dict[str, list]:
-    comments = load_comments_from_xml(file_path)
-    assert len(comments) == 9
+    comments = load_comments_from_xml(file_path, key="morph")
     seg_new_parents_map = {}
-    for comment in comments[5:]:
-        segi, new_parents = comment.split(":")
+    for comment in comments:
+        segi, new_parents = comment["key"], comment["parents"]
         seg_new_parents_map[segi] = eval(new_parents)
     return seg_new_parents_map
-
-
-def _marker_numbers_from_xml_file(file_path: str) -> dict[str, int]:
-    comments = load_comments_from_xml(file_path)
-    assert len(comments) == 9
-    seg_marker_map = {}
-    for comment in comments[:5]:
-        segi, marker_eq_number = comment.split(",")
-        seg_marker_map[segi] = int(marker_eq_number[-1])
-        # marker numbers are 1,2,3, or 4
-        assert 1 <= seg_marker_map[segi] <= 4
-    return seg_marker_map
 
 
 def load_sys(
@@ -89,10 +79,21 @@ def load_data(
     right_padd: float = 0.0,
     start_for_start: bool = True,
     stop_for_stop: bool = True,
+    resample_to_hz: float = 100.0,
 ) -> dict:
-    trial_data = joblib.load(_relative_to_this_file(f"joblib/{exp_id}.joblib"))
+    trial_data = joblib.load(
+        _relative_to_this_file(f"joblib/synced_aligned_nan_croptail_{exp_id}.joblib")
+    )
 
-    timings = _read_yaml("timings.yaml")[exp_id]["timings"]
+    metadata = _read_yaml("metadata.yaml")[exp_id]
+    timings = metadata["timings"]
+    hz_imu, hz_omc = float(metadata["hz"]["imu"]), float(metadata["hz"]["omc"])
+
+    trial_data = omc.resample(
+        trial_data,
+        hz_in=omc.hz_helper(trial_data.keys(), hz_imu=hz_imu, hz_omc=hz_omc),
+        hz_out=resample_to_hz,
+    )
 
     if motion_stop is None:
         motion_stop = motion_start
@@ -111,18 +112,33 @@ def load_data(
     t1 = max(t1, 0.0)
     t2 = timings[motion_stop]["stop" if stop_for_stop else "start"] + right_padd
 
-    # TODO
-    HZ = 100.0
-    trial_data = _crop_sequence(trial_data, 1 / HZ, t1=t1, t2=t2)
-
-    # xml_path = _relative_to_this_file(_id2xml[exp_id])
-    # seg_to_marker_number = _marker_numbers_from_xml_file(xml_path)
-
-    # TODO
-    # for seg in trial_data:
-    #    trial_data[seg]["pos"] = trial_data[seg][f"marker{seg_to_marker_number[seg]}"]
+    trial_data = _crop_sequence(trial_data, 1 / resample_to_hz, t1=t1, t2=t2)
 
     return trial_data
+
+
+def link_name_pos_rot_data(data: dict, xml_str: str) -> dict:
+    comments = load_comments_from_str(xml_str, key="omc")
+
+    data_out = dict()
+    for comment in comments:
+        bodyname, omcname, marker = (
+            comment["bodyname"],
+            comment["omcname"],
+            int(comment["marker"]),
+        )
+
+        if "pos" not in comment:
+            pos_offset = jnp.zeros((3,))
+        else:
+            pos_x, pos_y, pos_z = map(float, comment["pos"].split(","))
+            pos_offset = jnp.array([pos_x, pos_y, pos_z])
+        pos = data[omcname][f"marker{marker}"]
+        quat = data[omcname]["quat"]
+        pos_with_offset = pos + maths.rotate(pos_offset, quat)
+        data_out[bodyname] = dict(pos=pos_with_offset, quat=quat)
+
+    return data_out
 
 
 def _crop_sequence(data: dict, dt: float, t1: float = 0.0, t2: Optional[float] = None):
