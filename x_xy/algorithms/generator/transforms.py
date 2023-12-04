9912d1b8ac8270956866d0376f7af3d7670546ee
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2023-12-04 23:34:40 +0100

    set IMUs not noisy as default

--- x_xy/algorithms/generator/transforms.py
+++ x_xy/algorithms/generator/transforms.py
@@ -252,7 +252,7 @@ def _imu_data(key, xs, sys_xs, has_magnetometer) -> dict:
                 sys_xs.gravity,
                 sys_xs.dt,
                 consume,
-                noisy=True,
+                noisy=False,
                 low_pass_filter_pos_f_cutoff=13.5,
                 low_pass_filter_rot_alpha=0.5,
                 has_magnetometer=has_magnetometer,
