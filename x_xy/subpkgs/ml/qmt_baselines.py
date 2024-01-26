4d5e4e54d853de6e82d538e43db60b7733fd7309
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2024-01-26 21:36:52 +0100

    adds more baselines

--- x_xy/subpkgs/ml/qmt_baselines.py
+++ x_xy/subpkgs/ml/qmt_baselines.py
@@ -1,23 +1,47 @@
 from typing import Optional
 
+import jax
 import numpy as np
 import qmt
 
 import x_xy
 from x_xy.subpkgs import ml
 
+_attitude_methods = {
+    "vqf": ("VQFAttitude", qmt.oriEstVQF),
+    "madgwick": ("MadgwickAttitude", qmt.oriEstMadgwick),
+    "mahony": ("MahonyAttitude", qmt.oriEstMahony),
+    "seel": ("SeelAttitude", qmt.oriEstIMU),
+}
+
+
+class AttitudeBaseline(ml.AbstractFilter2d):
+    def __init__(self, method: str) -> None:
+        """ """
+        self._name, self._method = _attitude_methods[method]
+
+    def _predict_2d(self, X, sys):
+        quats = dict()
+        for name, imu_data in X.items():
+            quats[name] = self._method(
+                gyr=imu_data["gyr"], acc=imu_data["acc"], params=dict(Ts=float(sys.dt))
+            )
+
+        # NOTE CONVENTION !!
+        quats = {name: qmt.qinv(quats[name]) for name in quats}
+        return quats
+
 
 class TwoSeg1D(ml.AbstractFilter2d):
     def __init__(
         self,
-        hinge_joint_axis: np.ndarray,
-        name: str,
-        first_seg_name: str,
-        second_seg_name: str,
+        method: str,
+        seg: str,
+        hinge_joint_axis: np.ndarray | None = None,
         lpf_glo: Optional[float] = None,
         lpf_rel: Optional[float] = None,
     ):
-        """Estimates Inclination and Relative Pose for Hinge Joint.
+        """Estimates Inclination and Relative Pose for Hinge Joint. Uses 6D VQF.
 
         Args:
             hinge_joint_axis (np.ndarray): Known Hinge Joint Axis.
@@ -25,27 +49,56 @@ class TwoSeg1D(ml.AbstractFilter2d):
             first_seg_name (str): First segment name.
             second_seg_name (str): Second segment name.
         """
-        self.hinge_joint_axis = np.atleast_2d(hinge_joint_axis)[0]
+
+        self.hinge_joint_axis = None
+        if hinge_joint_axis is not None:
+            assert hinge_joint_axis.shape == (3,)
+            self.hinge_joint_axis = hinge_joint_axis
+        self.seg = seg
+        self.method = method
+        assert method in ["euler_2d", "euler_1d", "1d_corr", "proj"]
+        name = method + f"_ollson_{int(hinge_joint_axis is None)}"
         self._name = name
-        self.first, self.second = first_seg_name, second_seg_name
         self.lpf_glo, self.lpf_rel = lpf_glo, lpf_rel
 
     def _predict_2d(self, X, sys):
-        del sys
+        X = jax.tree_map(lambda arr: np.asarray(arr).copy(), X)
+
+        assert len(X) == 2
+        Ts = float(sys.dt)
 
         # VQF
         quats = dict()
         for name, imu_data in X.items():
             quats[name] = qmt.oriEstVQF(
-                imu_data["gyr"], imu_data["acc"], params=dict(Ts=0.01)
+                imu_data["gyr"], imu_data["acc"], params=dict(Ts=Ts)
             )
 
         # heading correction of second orientation estimate
-        first, second = self.first, self.second
+        first, second = self.seg, list(set(X.keys()) - set([self.seg]))[0]
         gyr1, gyr2 = X[first]["gyr"], X[second]["gyr"]
-        t = np.arange(gyr1.shape[0] / 100, step=0.01)
+
+        t = np.arange(gyr1.shape[0] * Ts, step=Ts)
+
+        if self.hinge_joint_axis is None:
+            # returns jhat1, jhat2 which both are shape = (3x1)
+            self.hinge_joint_axis = qmt.jointAxisEstHingeOlsson(
+                X[first]["acc"],
+                X[second]["acc"],
+                gyr1,
+                gyr2,
+                estSettings=dict(quiet=True),
+            )[0][:, 0]
+
         quats[second] = qmt.headingCorrection(
-            gyr1, gyr2, quats[first], quats[second], t, self.hinge_joint_axis, None
+            gyr1,
+            gyr2,
+            quats[first],
+            quats[second],
+            t,
+            self.hinge_joint_axis,
+            None,
+            estSettings=dict(constraint=self.method),
         )[0]
 
         # NOTE CONVENTION !!
@@ -62,9 +115,6 @@ class TwoSeg1D(ml.AbstractFilter2d):
         # add it such that it gets low-pass-filtered for `lpf_rel` too
         yhat[first] = quats[first]
         yhat = _maybe_lowpassfilter_quats(yhat, self.lpf_rel)
-
-        # inclination angle; eps to femur
-        yhat[first] = x_xy.maths.quat_project(yhat[first], np.array([0.0, 0, 1]))[1]
 
         return yhat
 
