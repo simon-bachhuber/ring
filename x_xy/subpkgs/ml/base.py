167af88f72b49654829e9b92d3c187a86aae5d49
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2024-01-27 02:46:31 +0100

    adds `ml.InitApplyFilter.set_sys`

--- x_xy/subpkgs/ml/base.py
+++ x_xy/subpkgs/ml/base.py
@@ -14,21 +14,21 @@ from .ml_utils import load
 
 
 class AbstractFilter(ABC):
-    def _predict_2d(self, X: dict, sys: x_xy.System | None) -> dict:
+    def _predict_2d(self, X: dict, sys: x_xy.System | None, **kwargs) -> dict:
         X = tree_utils.add_batch_dim(X)
-        y = self._predict_3d(X, sys)
+        y = self._predict_3d(X, sys, **kwargs)
         return tree_utils.tree_slice(y, 0)
 
     @abstractmethod
-    def _predict_3d(self, X: dict, sys: x_xy.System | None) -> dict:
+    def _predict_3d(self, X: dict, sys: x_xy.System | None, **kwargs) -> dict:
         pass
 
-    def predict(self, X: dict, sys: x_xy.System | None) -> dict:
+    def predict(self, X: dict, sys: x_xy.System | None, **kwargs) -> dict:
         "X.shape = (bs, timesteps, features) or (timesteps, features)"
         if tree_utils.tree_ndim(X) == 2:
-            return self._predict_2d(X, sys)
+            return self._predict_2d(X, sys, **kwargs)
         else:
-            return self._predict_3d(X, sys)
+            return self._predict_3d(X, sys, **kwargs)
 
     @property
     def name(self) -> str:
@@ -44,14 +44,14 @@ class AbstractFilter2d(AbstractFilter):
     "Same as `AbstractFilter` but have to define `_predict_2d`"
 
     @abstractmethod
-    def _predict_2d(self, X: dict, sys: System | None) -> dict:
+    def _predict_2d(self, X: dict, sys: System | None, **kwargs) -> dict:
         pass
 
-    def _predict_3d(self, X: dict, sys: System | None) -> dict:
+    def _predict_3d(self, X: dict, sys: System | None, **kwargs) -> dict:
         N = tree_utils.tree_shape(X)
         ys = []
         for i in range(N):
-            ys.append(self._predict_2d(tree_utils.tree_slice(X, i), sys))
+            ys.append(self._predict_2d(tree_utils.tree_slice(X, i), sys, **kwargs))
         return tree_utils.tree_batch(ys)
 
 
@@ -68,14 +68,26 @@ class InitApplyFnFilter(AbstractFilter):
         self._name = name
         self.key = key
         self.params = self._load_params(params)
+        self.init_apply_fn = None
         self.init_apply_fn_factory = init_apply_fn_factory
         self.lpf = lpf
         self.X_transform = X_transform
 
-    def _predict_3d(self, X: dict, sys: System | None) -> dict:
-        init_apply_fn = self.init_apply_fn_factory(sys)
-        params, state = init_apply_fn.init(self.key, X)
-        params = params if self.params is None else self.params
+    def _predict_3d(
+        self, X: dict, sys: System | None, params: dict | None = None
+    ) -> dict:
+        if sys is not None:
+            assert self.init_apply_fn is None
+            init_apply_fn = self.init_apply_fn_factory(sys)
+        else:
+            init_apply_fn = self.init_apply_fn
+
+        if params is None:
+            params, state = init_apply_fn.init(self.key, X)
+            params = params if self.params is None else self.params
+        else:
+            _, state = init_apply_fn.init(self.key, X)
+
         bs = tree_utils.tree_shape(X)
         state = jax.tree_map(lambda arr: jnp.repeat(arr[None], bs, axis=0), state)
 
@@ -93,6 +105,9 @@ class InitApplyFnFilter(AbstractFilter):
             )
 
         return yhat
+
+    def set_sys(self, sys):
+        self.init_apply_fn = self.init_apply_fn_factory(sys)
 
     @staticmethod
     def _load_params(params: str | tree_utils.PyTree | None):
