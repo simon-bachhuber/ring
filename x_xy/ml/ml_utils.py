9ee0b40116c999038b6675629b62568161a298c4
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2024-03-04 22:16:48 +0100

    remove concept of `MultimediaLogger`; improved `DictLogger`; removes `NeptuneLogger`

--- x_xy/ml/ml_utils.py
+++ x_xy/ml/ml_utils.py
@@ -1,69 +1,85 @@
-from abc import ABC
-from abc import abstractmethod
-from abc import abstractstaticmethod
+from collections import defaultdict
 from functools import partial
-import logging
 import os
 from pathlib import Path
 import pickle
 import random
 import time
-from typing import Optional
+from typing import Optional, Protocol
 
 import jax
 import numpy as np
 from tree_utils import PyTree
-from tree_utils import tree_batch
 
 import wandb
 import x_xy
 from x_xy.utils import import_lib
 
-# An arbitrarily nested dictionary with jax.Array leaves; Or strings
+# An arbitrarily nested dictionary with Array leaves; Or strings
 NestedDict = PyTree
 STEP_METRIC_NAME = "i_episode"
 
 
-class Logger(ABC):
-    @abstractmethod
-    def log(self, metrics: NestedDict):
-        pass
+class Logger(Protocol):
+    def close(self) -> None: ...  # noqa: E704
 
+    def log(self, metrics: NestedDict) -> None: ...  # noqa: E704
+
+    @staticmethod
+    def n_params(params) -> int:
+        "Number of parameters in Pytree `params`."
+        return sum([arr.flatten().size for arr in jax.tree_util.tree_leaves(params)])
+
+
+class MixinLogger(Logger):
     def close(self):
         pass
 
+    def log_image(self, path: str, caption: Optional[str] = None):
+        raise NotImplementedError
 
-def n_params(params) -> int:
-    "Number of parameters in Pytree `params`."
-    return sum([arr.flatten().size for arr in jax.tree_util.tree_leaves(params)])
+    def log_video(
+        self,
+        path: str,
+        fps: int = 25,
+        caption: Optional[str] = None,
+        step: Optional[int] = None,
+    ):
+        raise NotImplementedError
 
-
-class DictLogger(Logger):
-    def __init__(self, output_path: Optional[str] = None):
-        self._logs = {}
-        self._output_path = output_path
+    def log_params(self, path: str):
+        raise NotImplementedError
 
     def log(self, metrics: NestedDict):
-        metrics = _flatten_convert_filter_nested_dict(metrics, filter_nan_inf=False)
-        metrics = tree_batch([metrics])
+        step = metrics[STEP_METRIC_NAME] if STEP_METRIC_NAME in metrics else None
+        for key, value in _flatten_convert_filter_nested_dict(metrics).items():
+            self.log_key_value(key, value, step=step)
 
-        for key in metrics:
-            existing_keys = []
-            if key in self._logs:
-                existing_keys.append(key)
-            else:
-                self._logs[key] = metrics[key]
+    def log_key_value(self, key: str, value: str | float, step: Optional[int] = None):
+        raise NotImplementedError
 
-        if len(existing_keys) > 0:
-            self._logs.update(
-                tree_batch(
-                    [
-                        {key: self._logs[key] for key in existing_keys},
-                        {key: metrics[key] for key in existing_keys},
-                    ],
-                    True,
-                )
-            )
+    def log_command_output(self, command: str):
+        path = command.replace(" ", "_") + ".txt"
+        os.system(f"{command} >> {path}")
+        self.log_txt(path, wait=True)
+        os.system(f"rm {path}")
+
+    def log_txt(self, path: str, wait: bool = True):
+        raise NotImplementedError
+
+    def _log_environment(self):
+        self.log_command_output("pip list")
+        self.log_command_output("conda list")
+        self.log_command_output("nvidia-smi")
+
+
+class DictLogger(MixinLogger):
+    def __init__(self, output_path: Optional[str] = None):
+        self._logs = defaultdict(lambda: [])
+        self._output_path = output_path
+
+    def log_key_value(self, key: str, value: str | float, step: int | None = None):
+        self._logs[key].append(value)
 
     def close(self):
         if self._output_path is None:
@@ -74,163 +90,15 @@ class DictLogger(Logger):
         path = Path(path).with_suffix(".pickle").expanduser()
         path.mkdir(parents=True, exist_ok=True)
         with open(path, "wb") as file:
-            pickle.dump(self._logs, file, protocol=5)
+            pickle.dump(self.get_logs(), file, protocol=5)
 
-    @staticmethod
-    def load(path: str):
-        with open(path, "rb") as file:
-            logs = pickle.load(file)
-        logger = DictLogger(path)
-        logger._logs = logs
-        return DictLogger
+    def get_logs(self):
+        return self._logs
 
 
-class MultimediaLogger(Logger):
-    @abstractmethod
-    def log_image(self, path: str, caption: Optional[str] = None):
-        pass
-
-    @abstractmethod
-    def log_video(
-        self,
-        path: str,
-        fps: int = 25,
-        caption: Optional[str] = None,
-        step: Optional[int] = None,
-    ):
-        pass
-
-    @abstractmethod
-    def log_params(self, path: str):
-        pass
-
-    def log(self, metrics: NestedDict):
-        step = metrics[STEP_METRIC_NAME] if STEP_METRIC_NAME in metrics else None
-        for key, value in _flatten_convert_filter_nested_dict(metrics).items():
-            self.log_key_value(key, value, step=step)
-
-    @abstractmethod
-    def log_key_value(self, key: str, value: str | float, step: Optional[int] = None):
-        pass
-
-    def log_command_output(self, command: str):
-        path = command.replace(" ", "_") + ".txt"
-        os.system(f"{command} >> {path}")
-        self.log_txt(path, wait=True)
-        os.system(f"rm {path}")
-
-    @abstractmethod
-    def log_txt(self, path: str, wait: bool = True):
-        pass
-
-    @staticmethod
-    def _print_upload_file(path: str):
-        logging.info(f"Uploading file {path}.")
-
-    @abstractstaticmethod
-    def disable():
-        pass
-
-
-class MockMultimediaLogger(MultimediaLogger):
-    def log_image(self, path: str, caption: str | None = None):
-        pass
-
-    def log_video(
-        self,
-        path: str,
-        fps: int = 25,
-        caption: str | None = None,
-        step: int | None = None,
-    ):
-        pass
-
-    def log_params(self, path: str):
-        pass
-
-    def log_key_value(self, key: str, value: str | float, step: int | None = None):
-        pass
-
-    def log_txt(self, path: str, wait: bool = True):
-        pass
-
-    @staticmethod
-    def disable():
-        pass
-
-
-class NeptuneLogger(MultimediaLogger):
-    def __init__(
-        self,
-        project: Optional[str] = None,
-        name: Optional[str] = None,
-    ):
-        """Logger that logs the training progress to Neptune.
-
-        Args:
-            project (Optional[str], optional): Name of the project where the run should
-                go, in the form "workspace-name/project_name". Can also be provided
-                using the environemnt variable `NEPTUNE_PROJECT`
-            name (Optional[str], optional): Identifier inside the project. Can also be
-                provided using the environment variable `NEPTUNE_NAME`
-
-        Raises:
-            Exception: If environment variable `NEPTUNE_TOKEN` is unset.
-        """
-        api_token = os.environ.get("NEPTUNE_TOKEN", None)
-        if api_token is None:
-            raise Exception(
-                "Could not find the token for neptune logging. Make sure that the \
-                            environment variable `NEPTUNE_TOKEN` is set."
-            )
-
-        if name is None:
-            name = os.environ.get("NEPTUNE_NAME", None)
-
-        neptune = import_lib("neptune")
-
-        self.run = neptune.init_run(
-            name=name,
-            project=project,
-            api_token=api_token,
-        )
-
-        _log_environment(self)
-
-    def log_key_value(self, key: str, value: str | float, step: Optional[int] = None):
-        self.run[key].log(value)
-
-    def log_params(self, path: str):
-        self._print_upload_file(path)
-        # if we wouldn't wait then the run might end before upload finishes
-        self.run[f"params/{_file_name(path, extension=True)}"].upload(path, wait=True)
-
-    def log_video(
-        self,
-        path: str,
-        fps: int = 25,
-        caption: Optional[str] = None,
-        step: Optional[int] = None,
-    ):
-        self.run[f"video/{_file_name(path, extension=True)}"].upload(path)
-
-    def log_image(self, path: str, caption: Optional[str] = None):
-        self.run[f"image/{_file_name(path, extension=True)}"].upload(path)
-
-    def log_txt(self, path: str, wait: bool = True):
-        self.run[f"txt/{_file_name(path)}"].upload(path, wait=wait)
-
-    def close(self):
-        self.run.stop()
-
-    @staticmethod
-    def disable():
-        os.environ["NEPTUNE_MODE"] = "debug"
-
-
-class WandbLogger(MultimediaLogger):
+class WandbLogger(MixinLogger):
     def __init__(self):
-        _log_environment(self)
+        self._log_environment()
         wandb.run.define_metric(STEP_METRIC_NAME)
 
     def log_key_value(self, key: str, value: str | float, step: Optional[int] = None):
@@ -240,7 +108,6 @@ class WandbLogger(MultimediaLogger):
         wandb.log(data)
 
     def log_params(self, path: str):
-        self._print_upload_file(path)
         wandb.save(path, policy="now")
 
     def log_video(
@@ -269,28 +136,8 @@ class WandbLogger(MultimediaLogger):
         if wait:
             time.sleep(3)
 
-    @staticmethod
-    def disable():
-        os.environ["WANDB_MODE"] = "offline"
-
     def close(self):
         wandb.run.finish()
-
-
-def disable_syncing_to_cloud():
-    NeptuneLogger.disable()
-    WandbLogger.disable()
-
-
-def _file_name(path: str, extension: bool = False):
-    file = path.split("/")[-1]
-    return file if extension else file.split(".")[0]
-
-
-def _log_environment(logger: MultimediaLogger):
-    logger.log_command_output("pip list")
-    logger.log_command_output("conda list")
-    logger.log_command_output("nvidia-smi")
 
 
 def _flatten_convert_filter_nested_dict(
