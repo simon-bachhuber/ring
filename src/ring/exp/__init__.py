3465394fb3ae60788e2fd4b947f99a9f3861cd4a
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2024-03-20 13:55:05 +0100

    removes `ring.exp` and moves it to `sparsam` package

--- src/ring/exp/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import baselines
-from . import benchmark
-from . import exp
-from . import omc_utils
-from .benchmark import benchmark as benchmark_fn
-from .benchmark import IMTP
-from .exp import load_data
-from .exp import load_sys
--- src/ring/exp/baselines/__init__.py
+++
@@ -1,3 +0,0 @@
-from .qmt_baselines import AttitudeBaseline
-from .qmt_baselines import TwoSeg1D
-from .qmt_baselines import VQF_9D
