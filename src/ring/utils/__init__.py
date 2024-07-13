f915ba1af74393bbc31198ec3bbd47f9431f6813
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2024-07-13 13:11:27 +0200

    removes `ring.RCMG.to_hdf5`

--- src/ring/utils/__init__.py
+++ src/ring/utils/__init__.py
@@ -1,17 +1,16 @@
+from . import hdf5
+from . import randomize_sys
 from .batchsize import batchsize_thresholds
 from .batchsize import distribute_batchsize
 from .batchsize import expand_batchsize
 from .batchsize import merge_batchsize
 from .colab import setup_colab_env
-from .hdf5 import load as hdf5_load
-from .hdf5 import load_from_multiple as hdf5_load_from_multiple
-from .hdf5 import load_length as hdf5_load_length
-from .hdf5 import save as hdf5_save
 from .normalizer import make_normalizer_from_generator
 from .normalizer import Normalizer
 from .path import parse_path
 from .utils import dict_to_nested
 from .utils import dict_union
+from .utils import gcd
 from .utils import import_lib
 from .utils import pickle_load
 from .utils import pickle_save
